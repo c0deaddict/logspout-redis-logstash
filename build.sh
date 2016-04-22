5a1d0e350225819684b47af359da8bdaf16443fb
Jos van Bakel <jos@kalooga.com>
2016-04-22 09:27:25 +0200

    Change module path

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ package main
 import (
   _ "github.com/gliderlabs/logspout/httpstream"
   _ "github.com/gliderlabs/logspout/routesapi"
-  _ "github.com/rtoma/logspout-redis-logstash"
+  _ "github.com/c0deaddict/logspout-redis-logstash"
 )
 MODULES
 
-docker build -t rtoma/logspout-redis-logstash -f Dockerfile.example .
+docker build -t c0deaddict/logspout-redis-logstash -f Dockerfile.example .
 
 rm -f Dockerfile.example modules.go
